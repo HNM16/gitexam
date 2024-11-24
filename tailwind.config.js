e2755435e770d40fdc4d3e97224ce2008e1ecec7
16 <muhammad.nasridiov@icloud.com>
2024-11-25 03:47:42 +0500

    section7

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '390px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '440px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '680px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '880px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
