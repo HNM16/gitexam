d497294315bccccd586a3aabfc0041357c22e15a
16 <muhammad.nasridiov@icloud.com>
2024-11-25 19:31:51 +0500

    sect5

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
       'sm': '390px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '440px',
+      'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '680px',
+      'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '880px',
+      'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
